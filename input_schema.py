724e6d4498738a2482d72840d130688b9c1ed42c
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-10-08 19:57:01 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -3,6 +3,6 @@ INPUT_SCHEMA = {
         'datatype': 'STRING',
         'required': True,
         'shape': [1],
-        'example': ["How to count all the stars in the night sky?"]
+        'example': [""]
     }
 }
